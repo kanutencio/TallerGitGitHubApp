38d2dfa33f65bb6bdeafa511e0d2bf6a397497c2
Alejandro Pradilla Castro <apradilla796@unab.edu.co>
2026-09-16 15:01:45 -0500

    feat: foto personal

--- app/src/main/java/me/alejandropradilla/tallergitgithubapp/MainActivity.kt
+++ app/src/main/java/me/alejandropradilla/tallergitgithubapp/MainActivity.kt
@@ -4,13 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import me.alejandropradilla.tallergitgithubapp.ui.theme.TallerGitGitHubAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +38,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TallerGitGitHubAppTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
     }
 }
 
+@Preview(showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun PersonalCard(){
+    Column(modifier = Modifier
+        .fillMaxSize().padding(8.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
     )
-}
+    {
+        Image(
+            painter = painterResource(id = R.drawable.jeff),
+            contentDescription = "es jeff",
+            modifier = Modifier
+                .height(200.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.height(25.dp))
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TallerGitGitHubAppTheme {
-        Greeting("Android")
     }
 }
+
+
+
